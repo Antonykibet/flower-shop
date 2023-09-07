ebe47addfaee40893ead93658f9fc23ac999f2d0
Antonykibet <antonykibet059@gmail.com>
2023-09-07 16:19:56 +0300

    finalised hide user functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,24 @@
-let eyeIcon = document.querySelectorAll('.eyeIcon')
-let formInput = document .getElementsByClassName('.formInput')
-eyeIcon.forEach((button,index)=>{
-  button.addEventListener('click',()=>{
-    let input = formInput.item(index)
-    if(input.text=='password'){
-     formInput.item(index).setAttribute('type','text')
-    button.src=//hidden eye
-    }
-  })else{
-     formInput.item(index).setAttribute('type',password)
-  }
+let eyeIcon = document.getElementsByClassName('eyeIcon')
+let passwordInput = document .getElementById('password')
+let confirmPassInput = document .getElementById('confirmPassword')||null
+
+eyeIcon.item(0).addEventListener('click',()=>{
+    hidePass(passwordInput,'/icons/google icon.png','/icons/hide password icon.png',0)
 })
+
+if(confirmPassInput){
+    eyeIcon.item(1).addEventListener('click',()=>{
+        hidePass(confirmPassInput,'/icons/google icon.png','/icons/hide password icon.png',1)
+    })
+}
+
+function hidePass(input,unhideIcon,hideIcon,index){
+    if(input.type=='password'){
+        input.type='text'
+        eyeIcon.item(index).src=unhideIcon
+    }else{
+        input.type='password'
+        eyeIcon.item(index).src=hideIcon
+    }
+}
+
